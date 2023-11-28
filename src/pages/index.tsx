638bf3f7b2816d4bff2690278e147f2974700d5a
JefersonLRS <contato.jefersonlrs@gmail.com>
2023-11-28 20:53:09 -0300

    Header almost done :scream:

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,12 +1,24 @@
 import GreatButton from '@/components/GreatButton';
 import Header from '@/components/Header';
+import { FaArrowRightLong as Arrow } from 'react-icons/fa6';
 
 const Home = () => {
   return (
     <main className="min-h-screen bg-page-color">
       <Header />
-      <section className="h-screen m-auto flex justify-center items-center bg-header-cover">
-        <GreatButton>Nossas turmas</GreatButton>
+      <section className="h-screen m-auto flex flex-col justify-center items-center bg-header-cover bg-cover">
+        <div className="flex justify-start pl-24 items-center w-full">
+          <div className="text-white tracking-[-0.2em] mb-24">
+            <h1 className="text-7xl font-bold">SUPERE SEUS LIMITES</h1>
+            <h2 className="sub-title text-5xl font-bold text-transparent">
+              CONQUISTE NOVOS RESULTADOS
+            </h2>
+          </div>
+        </div>
+
+        <GreatButton>
+          Nossas turmas <Arrow />
+        </GreatButton>
       </section>
     </main>
   );
